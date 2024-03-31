c097aeb9ec9ef2e4ab57f82f18eb6765a0978db2
Alex Simisi Nta <smsxander@gmail.com>
2024-03-31 20:59:24 +0200

    affichage de la liste des produits

--- App.js
+++ App.js
@@ -1,20 +1,63 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from "react";
+import { Button, StyleSheet, Text, TextInput, View } from "react-native";
 
 export default function App() {
+  const [product, setProduct] = useState("");
+  const [list, setList] = useState([]);
+
+  const inputHandler = (val) => setProduct(val);
+
+  const submitHandler = () => {
+    setList((currentList) => [...currentList, product]);
+    setProduct("");
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.inputContainer}>
+        <TextInput
+          style={styles.textInput}
+          placeholder="Nouveau produit"
+          onChangeText={inputHandler}
+          value={product}
+        />
+        <Button title="valider" onPress={submitHandler} />
+      </View>
+      <View style={styles.items}>
+        {list.map((product, index) => (
+          <Text style={styles.element} key={index}>
+            {product}
+          </Text>
+        ))}
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    padding: 40,
+    paddingTop: 60,
+  },
+  inputContainer: {
+    flexDirection: "row",
+  },
+  textInput: {
+    borderColor: "gray",
+    borderWidth: 1,
+    padding: 5,
+    paddingLeft: 9,
+    fontSize: 16,
+    flexGrow: 1,
+  },
+  items: {
+    marginTop: 10,
+  },
+  element: {
+    backgroundColor: "#c0392b",
+    color: "#fff",
+    padding: 15,
+    fontSize: 16,
+    marginTop: 5,
   },
 });
