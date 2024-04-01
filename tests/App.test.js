54fd9c2066a49c8b9266b3968a6b52934b59c2f2
Alex Simisi Nta <117230604+alexdevops007@users.noreply.github.com>
2024-04-01 13:12:21 +0200

    Update App.test.js

---
+++ tests/App.test.js
@@ -0,0 +1,34 @@
+// App.test.js
+import React from "react";
+import { render, fireEvent } from "@testing-library/react-native";
+import App from "./App";
+
+test("doit afficher le champ de saisie et la liste de produits", () => {
+  const { getByPlaceholderText, getByText } = render(<App />);
+  const input = getByPlaceholderText("Nouveau produit");
+  const productList = getByText("Liste des produits");
+  expect(input).toBeTruthy();
+  expect(productList).toBeTruthy();
+});
+
+test("doit ajouter un produit à la liste lorsqu'un nouveau produit est soumis", () => {
+  const { getByPlaceholderText, getByText } = render(<App />);
+  const input = getByPlaceholderText("Nouveau produit");
+  const button = getByText("Valider");
+  fireEvent.changeText(input, "Pomme");
+  fireEvent.press(button);
+  const productItem = getByText("Pomme");
+  expect(productItem).toBeTruthy();
+});
+
+test("doit supprimer un produit de la liste lorsqu'un bouton de suppression est cliqué", () => {
+  const { getByPlaceholderText, getByText, queryByText } = render(<App />);
+  const input = getByPlaceholderText("Nouveau produit");
+  const addButton = getByText("Valider");
+  fireEvent.changeText(input, "Banane");
+  fireEvent.press(addButton);
+  const deleteButton = getByText("Supprimer");
+  fireEvent.press(deleteButton);
+  const deletedProductItem = queryByText("Banane");
+  expect(deletedProductItem).toBeNull();
+});
