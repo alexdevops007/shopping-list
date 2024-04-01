dc32c30f43c7896fdccce55c5068b93aca605629
Alex Simisi Nta <smsxander@gmail.com>
2024-04-01 02:33:39 +0200

    creation du dossier components

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import { Button, FlatList, StyleSheet, Text, TextInput, View } from "react-native";
 
 export default function App() {
